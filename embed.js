f1843e6ee4f67c86aa6cc9d784c066636d01fbc3
Roma Bulani <42478246+romabulani@users.noreply.github.com>
2022-02-03 17:47:28 +0530

    feat : added js for dismissable alerts

---
+++ embed.js
@@ -0,0 +1,5 @@
+      let btnCloseArray = document.querySelectorAll('.btn-close');
+      Array.from(btnCloseArray).map(btnClose => btnClose.addEventListener('click', onCrossClick));
+      function onCrossClick(){
+          this.parentNode.style.display= 'none';
+      }
