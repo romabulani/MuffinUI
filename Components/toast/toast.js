47374ec42f8950ae56e0a5f040a33552d85079b7
Roma Bulani <romabulani@gmail.com>
2022-02-07 20:56:59 +0530

    feat : Added Rating and Toast

---
+++ Components/toast/toast.js
@@ -0,0 +1,29 @@
+const btnCloseArray = document.querySelectorAll('.btn-close');
+
+const btnToastSuccess = document.querySelector('#btn-toast-success');
+const btnToastInfo = document.querySelector('#btn-toast-info');
+const btnToastWarning = document.querySelector('#btn-toast-warning');
+const btnToastError = document.querySelector('#btn-toast-error');
+
+const toastSuccess = document.querySelector('.toast-success');
+const toastInfo = document.querySelector('.toast-info');
+const toastWarning = document.querySelector('.toast-warning');
+const toastError = document.querySelector('.toast-error');
+
+btnToastSuccess.addEventListener('click', () => onClickHandler(toastSuccess));
+btnToastInfo.addEventListener('click', () => onClickHandler(toastInfo));
+btnToastWarning.addEventListener('click', () => onClickHandler(toastWarning));
+btnToastError.addEventListener('click', () => onClickHandler(toastError));
+
+const onClickHandler = element => {
+    console.log(element);
+    element.style.display = 'block';
+    setTimeout(() => {
+        element.style.display = 'none';
+    }, 3000);
+}
+
+Array.from(btnCloseArray).map(btnClose => btnClose.addEventListener('click', onCrossClick));
+function onCrossClick(){
+    this.parentNode.style.display= 'none';
+}
