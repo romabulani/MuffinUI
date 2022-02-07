0808484d00ed029e3eace47381400a45f1c12d89
Roma Bulani <42478246+romabulani@users.noreply.github.com>
2022-02-08 01:41:56 +0530

    Update embed.js

--- embed.js
+++ embed.js
@@ -27,3 +27,28 @@
                         element.style.display = 'none';
                   }, 3000);
             }
+
+            
+            
+            let addChip = document.querySelector('.chip-input');
+            let chipsCollector = document.querySelector('.chips-dynamic');
+
+
+            addChip.addEventListener('keyup', () => {
+            if (event.key === "Enter"){
+            const faTag = document.createElement('i');
+            faTag.classList.add('fas');
+            faTag.classList.add('fa-times-circle');
+            const newChip = document.createElement('span');
+            newChip.appendChild(document.createTextNode(addChip.value));
+            newChip.appendChild(faTag);
+            newChip.classList.add('chip');
+            faTag.addEventListener('click', onCrossClick);
+            chipsCollector.appendChild(newChip);
+            addChip.value='';
+          }
+        })
+
+function onCrossClick(){
+    this.parentNode.style.display= 'none';
+}
