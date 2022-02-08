8711e0331aafdec9ec27eedf706d0450bd8cc7fd
Roma Bulani <42478246+romabulani@users.noreply.github.com>
2022-02-08 18:33:10 +0530

    Update embed.js

--- embed.js
+++ embed.js
@@ -52,3 +52,18 @@
             function onCrossClick(){
                   this.parentNode.style.display= 'none';
             }
+// Tabs
+
+            const defaultTabsArray = document.querySelectorAll('.tab-default');
+            const leftIconsTabsArray = document.querySelectorAll('.tab-icon-left');
+            const topIconsTabsArray = document.querySelectorAll('.tab-icon-top');
+
+            defaultTabsArray.forEach(tab => tab.addEventListener('click', () => onClickTabHandler(defaultTabsArray,tab)));
+            leftIconsTabsArray.forEach(tab => tab.addEventListener('click', () => onClickTabHandler(leftIconsTabsArray,tab)));
+            topIconsTabsArray.forEach(tab => tab.addEventListener('click', () => onClickTabHandler(topIconsTabsArray,tab)));
+
+            const onClickTabHandler = (arr, activeTab) => {
+            arr.forEach(tab => tab.classList.remove('tab-active'));
+            activeTab.classList.add('tab-active');
+            }
+
