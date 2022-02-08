faa5ec803f4d4fba22a903b01b4e7e7abccf3b3d
Roma Bulani <romabulani@gmail.com>
2022-02-09 00:00:59 +0530

    fix : condition in chips

--- Components/chips/chips.js
+++ Components/chips/chips.js
@@ -3,18 +3,19 @@ let chipsCollector = document.querySelector('.chips-dynamic');
 
 
 addChip.addEventListener('keyup', () => {
-    if (event.key === "Enter"){
-        const faTag = document.createElement('i');
-        faTag.classList.add('fas');
-        faTag.classList.add('fa-times-circle');
-        const newChip = document.createElement('span');
-        newChip.appendChild(document.createTextNode(addChip.value));
-        newChip.appendChild(faTag);
-        newChip.classList.add('chip');
-        faTag.addEventListener('click', onCrossClick);
-        chipsCollector.appendChild(newChip);
-        addChip.value='';
-    }
+    if (event.key === "Enter")
+        if(addChip.value !== ''){
+            const faTag = document.createElement('i');
+            faTag.classList.add('fas');
+            faTag.classList.add('fa-times-circle');
+            const newChip = document.createElement('span');
+            newChip.appendChild(document.createTextNode(addChip.value));
+            newChip.appendChild(faTag);
+            newChip.classList.add('chip');
+            faTag.addEventListener('click', onCrossClick);
+            chipsCollector.appendChild(newChip);
+            addChip.value='';
+        }
 })
 
 function onCrossClick(){
