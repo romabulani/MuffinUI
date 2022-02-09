389353b128535bc1647da6534c62d29e24a539f4
Roma Bulani <romabulani@gmail.com>
2022-02-09 16:12:37 +0530

    fix : sidebar using JS

--- Documentation/sidebar.js
+++ Documentation/sidebar.js
@@ -1,9 +1,26 @@
-let hamburgerIcon = document.querySelector('.hamburger');
-let sidebar = document.querySelector('.sidebar');
+const hamburgerIcon = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
+const navIcons = Array.from(document.querySelectorAll('.nav-icon'));
+
+window.onresize = () => {
+    const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    if(vw > 640){
+        sidebar.style.display = 'block';
+        sidebar.style.minWidth = '14rem'
+        hamburgerIcon.style.visibility = 'hidden';
+        navIcons.map(navIcon => navIcon.style.display = 'inline-block');
+    }
+    else{
+        sidebar.style.display = 'none';
+        hamburgerIcon.classList.remove('fa-times');  
+        hamburgerIcon.classList.add('fa-bars');
+        hamburgerIcon.style.visibility = 'visible';
+        navIcons.map(navIcon => navIcon.style.display = 'none');
+    }
+};
 
 hamburgerIcon.addEventListener('click',() => {
     if (hamburgerIcon.classList.contains('fa-bars')){
-        // sidebar.style.transition = 'all 2s'
         sidebar.style.display = 'block';
         sidebar.style.minWidth = '100%'
         hamburgerIcon.classList.remove('fa-bars');  
@@ -11,8 +28,8 @@ hamburgerIcon.addEventListener('click',() => {
     }
     else{
         sidebar.style.display = 'none';
+        sidebar.style.minWidth = '14rem'
         hamburgerIcon.classList.remove('fa-times');  
         hamburgerIcon.classList.add('fa-bars'); 
-    }
-    
+    }   
 });
